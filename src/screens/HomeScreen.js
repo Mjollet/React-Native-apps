5751d5bbf4572e040a703eb570fa64a0a08d19cd
MJollet <48050378+Mjollet@users.noreply.github.com>
2020-04-23 12:10:30 +0200

    pink button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,37 +7,46 @@ const HomeScreen = ({ navigation }) => {
   <View>
 
     <Text style={styles.text}> Hi there ! </Text>
+
     <Button 
     onPress={() => navigation.navigate('Components')}
     title="Go to Component Demo"
+    color="#f194ff"
     />
     <Button 
     onPress={() => navigation.navigate('List')}
     title="Go to List Demo"
+    color="#f194ff"
     />
     <Button
     onPress={() => navigation.navigate('Image')}
     title="Go to Image Demo"
+    color="#f194ff"
     />    
      <Button
     onPress={() => navigation.navigate('Counter')}
     title="Go to Counter Demo"
+    color="#f194ff"
     />  
     <Button
     onPress={() => navigation.navigate('Color')}
     title="Go to Color Demo"
+    color="#f194ff"
     />  
     <Button
     onPress={() => navigation.navigate('Square')}
     title="Go to Square Demo"
+    color="#f194ff"
     />  
     <Button 
     onPress= { () => navigation.navigate('Text') }
     title="Go to Text Demo"
+    color="#f194ff"
     />
      <Button 
     onPress= { () => navigation.navigate('Box') }
     title="Go to Layout Demo"
+    color="#f194ff"
     />
   </View>
   );
@@ -45,9 +54,21 @@ const HomeScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+
+  viewStyle: {
+    borderWidth: 1,
+    borderColor: 'grey',
+
+  },
   text: {
-    fontSize: 30
-  }
+    padding: 5,
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+
 });
 
 export default HomeScreen;
